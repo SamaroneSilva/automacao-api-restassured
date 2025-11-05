e8be64caf898ede9bdd0834ca7a0c2d65023700d
SamaroneSilva <samarone.silva@outlook.com>
2025-11-04 22:24:14 -0300

    validação de performance com 600 ms

--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -244,7 +244,7 @@ public class ProductTest extends BaseTest {
         int totalRequisicoes = 15;
 
         // limite máximo aceitável (em milissegundos)
-        long limiteMs = 10;
+        long limiteMs = 600;
         List<Long> tempos = new ArrayList<>();
 
         System.out.println("🚀 Iniciando teste de performance com " + totalRequisicoes + " requisições...");
